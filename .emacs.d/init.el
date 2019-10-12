a7c15ae23802110ada27f2fe0a9d096d5307a0e3
ragingscholar <ragingscholar@protonmail.com>
2019-10-12 23:23:46 +0800

    working on conf.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,3 +15,17 @@
   (package-install 'use-package))
 
 (org-babel-load-file "~/.emacs.d/conf.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (moody which-key web-mode use-package sublime-themes solarized-theme smex rust-mode powerline-evil org-link-minor-mode org-bullets nord-theme multi-term mastodon markdown-mode htmlize helpful flycheck flx evil-surround evil-org evil-magit dumb-jump dired-hide-dotfiles diminish diff-hl counsel composer company-php auto-compile ag ac-php))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
